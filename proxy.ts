b552303c8d9c87f43fdde4c0b9c2eb4ff91c3f88
ShrijaySharma <kanhasharma313@gmail.com>
2026-07-11 18:40:35 +0530

    fix: rename middleware to proxy.ts and use proxy function to support nextjs 16

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,5 @@
 import { NextResponse, type NextRequest } from 'next/server'
-
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   let response = NextResponse.next({
     request,
   })
